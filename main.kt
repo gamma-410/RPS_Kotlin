4b1e1f700311e776c10008b1838def76462c6b97
gamma-410 <mail@dr-notes.work>
2022-01-13 02:17:02 +0900

    💎 とりあえず入力した rps を識別して置換できた。

--- main.kt
+++ main.kt
@@ -1,27 +1,46 @@
-
 fun main() {
+    println("ゲームを開始します。(開始: s) (終了: e)")
+    val text = readLine()
+    when (text) {
+        "s" -> {
+            start()
+        }
+        "e" -> {
+            return
+        }
+        else -> {
+            println("💡: (開始: s) (終了: e) から選択してください...")
+            return main()
+        }
+    }
+}
+
+fun start() {
     println("== ✊(r) ✋(p) ✌️(s) から選択 (終了: e) ==")
     val user_answer_before = readLine()
 
     when (user_answer_before) {
         "r" -> {
             val user_answer_after = user_answer_before.replace("r", "✊")
-            print("You: ${user_answer_after} (Rock)")
+            println("You: ${user_answer_after} (Rock)")
         }
         "p" -> {
             val user_answer_after = user_answer_before.replace("p", "🖐")
-            print("You: ${user_answer_after} (Paper)")
+            println("You: ${user_answer_after} (Paper)")
         }
         "s" -> {
             val user_answer_after = user_answer_before.replace("s", "✌️")
-            print("You: ${user_answer_after} (Scissors)")
+            println("You: ${user_answer_after} (Scissors)")
         }
         "e" -> {
             println("プログラムを終了しました。")
+            return
         }
         else -> {
-            println("💡: ✊(r) ✋(p) ✌️(s) の中から選択してください...")
-            return main()
+            println("\n💡: ✊(r) ✋(p) ✌️(s) の中から選択してください...\n")
+            return start()
         }
     }
+
+    return start()
 }
